a01b6810cde25938144c6ca921279db86854cbc5
basurakid <92126346+basurakid@users.noreply.github.com>
2026-06-18 16:56:14 +0200

    Implement basic API use and start working on html

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
+import './style.css'
 
+document.querySelector("#search-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const value = document.querySelector(".search-input").value;
+
+    if(value.trim()){
+        console.log(value)
+    } else {
+        console.log("Please input a location")
+    }
+})
+
+async function getWeatherData(location) {
+    try {
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=B7FH5FXVK8D8QGYND99MCZBT6&contentType=json`);
+        if (!response.ok) {
+            throw new Error("Something went wrong: " + response.status);
+        }
+        return await response.json();
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function cleanupData(weatherData) {
+    return {
+        temp: weatherData.feelslike,
+        precip: weatherData.precipprob,
+        conditions: weatherData.conditions,
+    }
+}
 
--- webpack.common.js
+++ webpack.common.js
@@ -1,23 +1,21 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
+  entry: {
+    app: './src/index.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html",
+      title: 'Production',
+      template: './src/template.html',
     }),
   ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
   module: {
     rules: [
       {
@@ -39,4 +37,3 @@ module.exports = {
     ],
   },
 };
-
